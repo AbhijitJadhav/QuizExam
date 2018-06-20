c5c85c39b78f5797b3cf42b6f7e02abb481b3ae6
ATMECS\Abhijit.Jadhav <abhijit.jadhav@atmecs.com>
2018-06-20 15:09:53 +0530

    Template created

---
+++ QuizExam/src/main/java/com/abhi/quizexam/QuizExamApplication.java
@@ -0,0 +1,12 @@
+package com.abhi.quizexam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizExamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizExamApplication.class, args);
+	}
+}
